7a7ba1a3ddbb75d0f20b1f69612384a623291a7b
santokiya <santokiya@gmail.com>
2018-01-06 11:57:12 -0600

    bamazon

---
+++ bamazon.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+items_id INTEGER AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NULL,
+department_name VARCHAR(100) NULL, 
+price DECIMAL(10,2) NULL,
+stock_quantity INTEGER NULL,
+PRIMARY KEY (items_id)
+);
+
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ('Charmin toilet paper', 'Bathroom', 75.00, 120);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ('Canon printer', 'Office', 50.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ('Boots', 'Shoes', 100.00, 55);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ('Snoopy T-shirt', 'Clothes', 14.99, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ('Ozarka water', 'Beverages', 5.99, 200);
+
+
+
+select * From bamazon_db;
---
+++ bamazonCustomer.js
@@ -0,0 +1,67 @@
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+
+    user: 'root',
+
+    password: '',
+    database: 'bamazon_db'
+})
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log('connected as id ' + connection.threadId)
+        // connection.end()
+    displayItems()
+})
+
+function displayItems() {
+    connection.query('SELECT * FROM products', function(err, results) {
+        if (err) throw err
+
+        var items = []
+        for (var i = 0; i < results.length; i++) {
+            // items.push(results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity)
+            console.log(results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity)
+        }
+    })
+}
+
+// once you have the items, prompt the user for which they'd like to buy
+inquirer
+    .prompt([{
+            name: 'choice',
+            type: 'input',
+
+            message: 'Please input the id number of the item you would like to buy.'
+        },
+        {
+            name: 'units',
+            type: 'input',
+            message: 'How many units would you like to buy?'
+        }
+    ])
+    .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem
+            // for (var i = 0; i < results.length; i++) {
+            //     if (results[i].item_name === answer.choice) {
+            //         chosenItem = results[i]
+            //     }
+            // }
+
+        console.log(answer.choice)
+        console.log(answer.units)
+
+        connection.query(`SELECT * FROM products WHERE id = ${answer.choice}`, function(err, results) {
+            // if (err) throw err
+            // console.log(results)
+            // console.log(results.stock_quantity)
+
+            if (results[0].stock_quantity < answer.units)
+                console.log('Insufficient Quantity')
+        })
+    })
